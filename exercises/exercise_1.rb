4abf1a0aeb3401c345e187783bbe9fb3054599d2
DuyguY <duygu.yldrm4820@gmail.com>
2022-02-01 21:43:40 -0500

    One-to-many association

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,14 +6,14 @@ puts "----------"
 # Your code goes below here ...
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true,
 womens_apparel: true)
+p burnaby
 
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false,
 womens_apparel: true)
+p richmond
 
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true,
 womens_apparel: false)
-
-p burnaby
-p richmond
 p gastown
+
 p Store.count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Tony", last_name: "Stark", hourly_rate: 80)
+@store1.employees.create(first_name: "Thor", last_name: "Ragnarok", hourly_rate: 70)
+@store2.employees.create(first_name: "Steve", last_name: "Rogers", hourly_rate: 90)
+@store2.employees.create(first_name: "Bruce", last_name: "Banner", hourly_rate: 50)
+@store2.employees.create(first_name: "Stan", last_name: "Lee", hourly_rate: 40)
