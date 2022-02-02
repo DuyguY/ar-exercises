078274cb9462bc58aa19c8cb2cbec83c540227a5
DuyguY <duygu.yldrm4820@gmail.com>
2022-02-01 21:28:46 -0500

    Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.all.sum(:annual_revenue)
+p @total_revenue
+
+@average_annual_revenue = Store.average(:annual_revenue).round
+p @average_annual_revenue
+
+@rich_stores = Store.where("annual_revenue >?", 1000000).size
+p @rich_stores
